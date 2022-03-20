f26a090101f8cb58fdf0872bafad1f924c8cb1ef
Abhishek-Poojary <poojaryabhishek13@gmail.com>
2022-03-20 19:27:37 +0530

    ErrorLogger-RequestLogger

--- sahara-backend/src/app.js
+++ sahara-backend/src/app.js
@@ -1,7 +1,14 @@
 const express=require("express");
 const app=express();
+const MyRequestLogger=require("./utilities/RequestLogger");
+const myErrorLogger = require( './utilities/ErrorLogger' );
 
+app.use(MyRequestLogger);
 app.use(express.json());
 
+
+
+
+app.use(myErrorLogger);
 
 module.exports=app;
