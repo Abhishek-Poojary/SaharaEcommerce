8e6d0841aa59163bb2e6b892914b3d10957e9dcd
Abhishek-Poojary <poojaryabhishek13@gmail.com>
2022-05-08 09:49:38 +0530

    UI Update For Update Order Page

--- sahara-frontend/src/component/Admin/OrderUpdate.js
+++ sahara-frontend/src/component/Admin/OrderUpdate.js
@@ -1,91 +1,105 @@
-import { Fragment, useEffect,useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Col, Row,Button } from 'react-bootstrap'
+import { Button, Table,Container } from 'react-bootstrap'
 import { getUserOrderDetails, updateOrderAdmin } from "../../actions/orderAction";
 import { REQUEST_PROCESS_ORDER_ADMIN_RESET } from "../../constants/OrderConstants";
 
+
 const OrderUpdate = () => {
-    const navigate =useNavigate();
+    const navigate = useNavigate();
     const { id } = useParams();
     const { loading, order } = useSelector((state) => state.orderDetails)
-    const { loading:updateLoading, status:update } = useSelector((state) => state.processOrder)
+    const { loading: updateLoading, status: update } = useSelector((state) => state.processOrder)
     const dispatch = useDispatch();
     const [status, setStatus] = useState("");
 
     useEffect(() => {
 
-        if(update){
+        if (update) {
             navigate('/admin/dashboard')
-            dispatch({type:REQUEST_PROCESS_ORDER_ADMIN_RESET})
+            dispatch({ type: REQUEST_PROCESS_ORDER_ADMIN_RESET })
         }
         dispatch(getUserOrderDetails(id))
 
-    }, [dispatch, id,update])
+    }, [dispatch, id, update])
 
 
-    const updateOrder=(e)=>{
+    const updateOrder = (e) => {
         e.preventDefault();
-        const data={
+        const data = {
             status,
         }
-        dispatch(updateOrderAdmin(id,data))
+        dispatch(updateOrderAdmin(id, data))
     }
 
     return (
         <Fragment>
 
             {loading === false && (
+                <div className="customOrderList">
+                    <Container>
 
-                <Row>
-                    <Col>
-                        <h2>Order Items</h2>
-                        {order && order.orderItems.map((data) => (
-                            <div key={data._id}>
-                                <h4>{data.name}</h4>
-                                <h4>{data.price}</h4>
-                                <h4>{data.quantity}</h4>
-                                <Link to={`/product/${data.product}`}>
-                                    {data.name}
-                                </Link>
-                            </div>
-                        ))}
-                    </Col>
-                    <Col>
-                        {order.totalPrice}
-                    </Col>
-                    <Col>
+                        <Table responsive>
+                            <thead>
+                                <tr>
+                                    <th><p className="customTitleUserOrder-1">Ordered Items</p></th>
+                                    <th><p className="customTitleUserOrder-1">Total Price</p></th>
+                                    <th><p className="customTitleUserOrder-1">Update Status</p></th>
+                                    <th><p className="customTitleUserOrder-1">OrderStatus</p></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr >
+                                    <td>
 
-                    </Col>
-                    <Col>
-                        <h1>{order.orderStatus}</h1>
-                    </Col>
-                    <Col>
+                                        {order && order.orderItems.map((data) => (
+                                            <div key={data._id}>
+                                                <p className="customTitleOrderList-1">{data.name}</p>
+                                                <p className="customTitleOrderList-1">{data.price}</p>
+                                                <p className="customTitleOrderList-1">{data.quantity}</p>
+                                                {/* <Link to={`/product/${data.product}`}>
+                                                    <Button  variant="primary mt-3">View Product</Button>
+                                                </Link> */}
+                                            </div>
+                                        ))}
+                                    </td>
+                                    <td>
+                                        <p className="customTitleOrderList-1">$ {order.totalPrice}</p>
+                                    </td>
+                                    <td>
+                                        <p className="customTitleOrderList-1">{order.orderStatus}</p>
 
-                        <form onSubmit={updateOrder}>
-                            <h1>Process Order</h1>
+                                    </td>
+                                    <td>
+                                        <form onSubmit={updateOrder}>
+                                            <div className="customOrderUpdate-1">
 
-                            <div>
-                               
-                                <select onChange={(e) => setStatus(e.target.value)}>
-                                    <option value="">Choose Category</option>
-                                    {order.orderStatus === "Processing" && (
-                                        <option value="Shipped">Shipped</option>
-                                    )}
+                                                <select onChange={(e) => setStatus(e.target.value)} >
+                                                    <option value="">Choose Category</option>
+                                                    {order.orderStatus === "Processing" && (
+                                                        <option value="Shipped">Shipped</option>
+                                                    )}
 
-                                    {order.orderStatus === "Shipped" && (
-                                        <option value="Delivered">Delivered</option>
-                                    )}
-                                </select>
-                            </div>
+                                                    {order.orderStatus === "Shipped" && (
+                                                        <option value="Delivered">Delivered</option>
+                                                    )}
+                                                </select>
+                                            </div>
 
-                            <Button type="submit" > Process </Button>
-                        </form>
-                    </Col>
-                </Row>
+                                            <Button type="submit" variant="primary mt-3"> Process </Button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Container>
 
+                </div>
             )}
+
+
         </Fragment>
     )
 }
